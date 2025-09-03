b3f7fac18e18ded1320f01c20d865efc96270802
Amanpreet Singh Malhotra <amanpreet21103@gmail.com>
2025-09-03 12:21:52 +0530

    Version 2

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/",async(req, res)=>{
-    res.json({
-        msg : "Hello, World"
-    });
+app.get("/", async (req, res) => {
+  res.json({
+    msg: "Hello, World",
+  });
+});
+
+app.post("/signup", async (req, res) => {
+  const {username, email} = req.body;
+  res.json({
+    username: username,
+    email: email,
+  });
 });
 
 app.listen(PORT, console.log(`Website is hosted on http://localhost:3000`));
